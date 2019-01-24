c3c41ae540712d9332844251a18bef03f7169ee7
Robby Klein <hello@robbyk.com>
2019-01-23 19:42:06 -0800

    Initial setup

---
+++ Funny/controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeController.swift
+//  Funny
+//
+//  Created by Robby Klein on 1/23/19.
+//  Copyright © 2019 Robby Klein. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
